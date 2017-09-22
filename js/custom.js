d4ec9a7b3b876686f22f28cc571608b7ddac0196
dillbala <mailtobalamurugan3@gmail.com>
2017-09-22 18:11:37 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+//smooth scroll
+var $window = $(window);		//Window object
+	
+	var scrollTime = 0.5;			//Scroll time
+	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+		
+	$window.on("mousewheel DOMMouseScroll", function(event){
+		
+		event.preventDefault();	
+										
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+			
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5							
+			});
+					
+	});
+
+
+
+//qty increcer
+  $('.spinner .btn:first-of-type').on('click', function() {
+    $('.spinner input').val( parseInt($('.spinner input').val(), 10) + 1);
+  });
+  $('.spinner .btn:last-of-type').on('click', function() {
+  	var qty = $('.spinner input').val();
+  	if(qty > 1){
+    	$('.spinner input').val( parseInt($('.spinner input').val(), 10) - 1);
+	}
+  });
+});
